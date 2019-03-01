183842f2b9469d7faf80fa8f8aa215558d9e2897
Alexei Fedotov <alexei.fedotov@gmail.com>
2019-03-01 14:12:40 +0300

    added cleanup on successful package build

---
+++ builddeps/opt/builddeps/pkgbuild.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+test -n "$1" || {
+  echo Usage: $0 [package name]
+  exit -1
+}
+
+dir0=`dirname "$0"`
+dir1="$dir0"/build/$1
+mkdir -p "$dir1"
+cd "$dir"
+
+apt-get source $1
+apt-get build-dep -y $1
+cd */debian
+DEB_BUILD_OPTIONS=nocheck debuild -b -uc -us
+apt-get install -y $1
+dpkg -i ../../$1_*.deb
+
+# if successful
+cd "$dir0"
+rm -rf build/"$1" &
+
